7d21d8e963c4fd6bde724def4880ca6ec07a2672
Oleg Mamin <oleg0x0@gmail.com>
2019-08-17 17:42:16 +0300

    added another sout

--- src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by AHYC on 17.04.2018.
- */
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("sdlfnsdknds");
-        System.out.println("Some letters");
-    }
-}
